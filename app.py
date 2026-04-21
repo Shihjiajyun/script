9d703acd1b044f1b8ccb53fa80e9bbaf302abb61
shihjiajyun1208 <shihjiajyun@gmail.com>
2026-04-21 16:48:21 +0800

    Remove GitHub link from footer in app.py to streamline content.

--- app.py
+++ app.py
@@ -504,6 +504,5 @@ if uploaded_file is not None:
 st.markdown("""
 <div class="site-footer">
     <a href="mailto:shihjiajyun@gmail.com">shihjiajyun@gmail.com</a>
-    <a href="https://github.com/Shihjiajyun/script" target="_blank">GitHub</a>
 </div>
 """, unsafe_allow_html=True)
